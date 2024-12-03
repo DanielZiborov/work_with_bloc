07d20f11a40d247ae706c739193f596122699f56
DanielZiborov <ziborovdanila5@gmail.com>
2024-12-03 23:48:17 +0300

    initial state

--- lib/moduls/dogs/bloc/dogs_state.dart
+++ lib/moduls/dogs/bloc/dogs_state.dart
@@ -3,6 +3,8 @@ part of 'dogs_bloc.dart';
 @immutable
 abstract class DogsState {}
 
+final class DogsInitialState extends DogsState {}
+
 final class DogsLoadingState extends DogsState {}
 
 final class DogsSuccessState extends DogsState {
