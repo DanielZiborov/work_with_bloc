d84281466ebb1e966a1d8c2818e6a563ecaf77bd
DanielZiborov <ziborovdanila5@gmail.com>
2024-12-04 19:42:17 +0300

    сущность для кошек

---
+++ lib/domain/entity/cats_entity.dart
@@ -0,0 +1,13 @@
+class CatsEntity {
+  final String id;
+  final String url;
+  final num width;
+  final num height;
+
+  CatsEntity({
+    required this.id,
+    required this.url,
+    required this.width,
+    required this.height,
+  });
+}
